e262a02a65ce4beefa354c7a7a08ce7b9123c20c
MISUBEIMP\Fantasia <md.rakib.hossain.misu@gmail.com>
2016-11-19 13:08:37 +0600

    clear unnecessary

--- ModuleBuildermisu.controller.js
+++
@@ -1,8 +0,0 @@
-(function (angular) {
-    'use strict';
-    function constructor() {
-        var vm = this;
-    }
-    constructor.$inject = [];
-    angular.module('moduelName').controller('misuController', constructor);
-})(angular.window);
