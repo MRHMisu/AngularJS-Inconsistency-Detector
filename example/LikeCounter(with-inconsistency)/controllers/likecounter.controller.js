d6a18d918022a13096f42f78ebfb17db39f3f122
MisuBeImp <bsse0516@iit.du.ac.bd>
2017-06-24 04:12:42 +0600

    Initialize ReadMe

--- example/LikeCounter(with-inconsistency)/controllers/likecounter.controller.js
+++ example/LikeCounter(with-inconsistency)/controllers/likecounter.controller.js
@@ -5,7 +5,15 @@
         vm.appTitle ="Like Counter";
         vm.count="0";
         vm.people=[{name:"Misu"},{name:"Rakib"}];
+
+function getCount()
+	{ vm.count=vm.count+1;
+		return vm.count;
+	}
+vm.getCount=getCount;
     }
+
+	
     constructor.$inject =[];
     angular.module('likecounter').controller('likecounterController',constructor);
 })();
